f0dc1a79e329dd5dc6b164ffb0bdfe22045ff46f
Moroni Perez <66393582+Aminadi2296@users.noreply.github.com>
2026-06-27 09:28:55 -0400

    fix: add node adapter for SSR

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'static',
-  integrations: [
-    react(),
-    tailwind(),
-  ],
-  server: {
-    port: 4321,
-  },
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
+  integrations: [react(), tailwind()],
 });
